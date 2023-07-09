2d5ef3b456de5c961c62a75b4dfdb8d83601c992
hyuntaekim1738 <hyuntaekim1738@gmail.com>
2023-07-09 13:14:55 -0500

    UI complete

--- vite-project/src/components/Home.tsx
+++ vite-project/src/components/Home.tsx
@@ -1,20 +1,21 @@
 import Post from './post/Post';
-
 const Home = () => {
     return (
         <>
-        <div className="container-fluid">
-            <h1>Home Page</h1>
-            <div className="feed">
-                <Post></Post>
-                <Post></Post>
-                <Post></Post>
-                <Post></Post>
-                <Post></Post>
-                <Post></Post>
-                <Post></Post>
+            <div className="container-fluid">
+                <h1>Home Page</h1>
+                <div className="feed">
+                    <div className="row d-flex justify-content-center"><Post></Post></div>
+                    <div className="row d-flex justify-content-center"><Post></Post></div>
+                    <div className="row d-flex justify-content-center"><Post></Post></div>
+                    <div className="row d-flex justify-content-center"><Post></Post></div>
+                    <div className="row d-flex justify-content-center"><Post></Post></div>
+                    <div className="row d-flex justify-content-center"><Post></Post></div>
+                    <div className="row d-flex justify-content-center"><Post></Post></div>
+                    <div className="row d-flex justify-content-center"><Post></Post></div>
+                </div>
+
             </div>
-        </div>
         </>
     )
 }
