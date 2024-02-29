b5078e83382ed7c9724e2e01eca9d99b439e9d8f
hyuntaekim1738 <hyuntaekim1738@gmail.com>
2024-02-29 09:21:07 -0600

    New Readme

---
+++ README.md
@@ -0,0 +1,18 @@
+# React Photoboard
+
+#### Purpose
+This project was developed for the purpose of learning full stack web development. 
+The project utilizes React Typescript, Google Firebase, and Bootstrap.
+
+#### Installation and Running
+Development environment requires node 16 or higher.
+Run "npm create vite@latest" and select React and then TypeScript. Follow the given instructions.
+Run "npm install react-router-dom" to get React Routing.
+Run using "npm run dev" command to run the program.
+Uses bootstrap using the command "npm install bootstrap@latest". 
+Run "npm install bootstrap-icons" to get the icons.
+
+#### Implementation History
+Implementation began with UI implementation which includes designing the React componenets and
+styling them with bootstrap. Implementation is currently in the stages of adding backend processes
+with Google Firebase.
--- vite-project/src/components/post/AddPost.tsx
+++ vite-project/src/components/post/AddPost.tsx
@@ -1,9 +1,31 @@
+import { useState, useEffect } from "react";
+
+interface Post{
+    id: string;
+    caption: string;
+    photos: string[];
+}
+
 const AddPost = () => {
-    interface Post{
-        id: string;
-        caption: string;
-        photos: string[];
-    }
+    const [formData, setFormData] = useState<Post>({
+        id: "",
+        caption: "",
+        photos: [""]
+    });
+
+    const handleCaptionChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { id, value } = e.target;
+        setFormData((prevData) => ({ ...prevData, [id]: value }));
+    };
+
+    const handlePictureChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const files = e.target.files?.[0]; // Get the selected file from the input
+        if (files) {
+            // Store the selected file in the state
+            console.log("Setting profile url");
+            setFormData((prevData) => ({ ...prevData, profileFile: files }));
+        }
+    };
     return (
         <>
             <div className="container-fluid">
@@ -14,7 +36,7 @@ const AddPost = () => {
                         <label htmlFor="photoUpload">Upload Photos:</label>
                     </div>         
                     <div className="form-floating mb-3">
-                        <input type="text" className="form-control" id="captionInput" placeholder="Enter caption"></input>
+                        <input type="text" className="form-control" id="caption" placeholder="Enter caption" onChange={handleCaptionChange}></input>
                         <label htmlFor="captionInput">Caption:</label>
                     </div>
                     <div className="form-floating">
